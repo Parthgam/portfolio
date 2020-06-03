24ad2b6ca7e3e441da6fd49c20a145341955c43a
Parthgam <parth.bsr@gmail.com>
2020-06-04 02:49:15 +0530

    skills and timeline svg changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,6 +3,10 @@ import { Grid } from '@material-ui/core';
 import Avatar from '../static/images/avatartemp.svg';
 import data from './data/skills.json';
 import SkillMan from '../static/images/skill-man.svg';
+import SkillManLeft from '../static/images/skills/man-left.svg';
+import SkillManRight from '../static/images/skills/man-right.svg';
+import BottomBase from '../static/images/skills/bottombase.svg';
+
 import SkillManBottom from '../static/images/skill-man-bottom.svg';
 
 export default function Skills() {
@@ -32,41 +36,76 @@ export default function Skills() {
             justify='center'
             alignItems='flex-end'
           >
-            <Grid item xs={2} md={2}>
-              <img src={SkillMan} />
+            <Grid item xs={2} md={2} className='skills-left'>
+              <img src={SkillManLeft} />
             </Grid>
-            <Grid item xs={10} md={10} className='skills-right'>
-              <Grid container direction='row' justify='flex-start'>
-                {data.map((value, index) => {
-                  return (
-                    <Grid item lg={3} md={3} xs={12}>
-                      <div className='skill-item'>
-                        <Grid container>
-                          <Grid item style={{ height: '2em', width: '2em' }}>
-                            <img
-                              src={require('../static/images/skills' +
-                                value.svgpath)}
-                            />
-                          </Grid>
-                          <Grid item style={{ height: '2em' }}>
-                            <Grid
-                              container
-                              direction='row'
-                              justify='center'
-                              alignItems='center'
-                              style={{ height: '2em' }}
-                            >
-                              <Grid item style={{ marginLeft: '0.5em' }}>
-                                {value.skillname}
+            <Grid item xs={8} md={8} style={{ padding: '20px' }}>
+              <Grid container className='skills-center-box'>
+                <Grid item xs={12} md={12}>
+                  <Grid container direction='row' justify='flex-end'>
+                    <Grid item xs={12} md={12} className='window-nav'>
+                      <Grid container direction='row' justify='flex-end'>
+                        <Grid item>
+                          <span
+                            class='dot'
+                            style={{ backgroundColor: '#FF3939' }}
+                          ></span>
+                          <span
+                            class='dot'
+                            style={{ backgroundColor: '#FFE249' }}
+                          ></span>
+                          <span
+                            class='dot'
+                            style={{ backgroundColor: '#9BE461' }}
+                          ></span>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid
+                    container
+                    direction='row'
+                    justify='flex-start'
+                    className='skills-right'
+                  >
+                    {data.map((value, index) => {
+                      return (
+                        <Grid item lg={3} md={3} xs={6}>
+                          <div className='skill-item'>
+                            <Grid container>
+                              <Grid
+                                item
+                                style={{ height: '2em', width: '2em' }}
+                              >
+                                <img
+                                  src={require('../static/images/skills' +
+                                    value.svgpath)}
+                                />
+                              </Grid>
+                              <Grid item style={{ height: '2em' }}>
+                                <Grid
+                                  container
+                                  direction='row'
+                                  justify='center'
+                                  alignItems='center'
+                                  style={{ height: '2em' }}
+                                >
+                                  <Grid item style={{ marginLeft: '0.5em' }}>
+                                    {value.skillname}
+                                  </Grid>
+                                </Grid>
                               </Grid>
                             </Grid>
-                          </Grid>
+                          </div>
                         </Grid>
-                      </div>
-                    </Grid>
-                  );
-                })}
+                      );
+                    })}
+                  </Grid>
+                </Grid>
               </Grid>
+            </Grid>
+            <Grid item xs={2} md={2} className='skills-left'>
+              <img src={SkillManRight} />
             </Grid>
           </Grid>
         </Grid>
